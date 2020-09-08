a94e03a328eea4d0a00d183d8d7de9b03d91813f
benjamin ausensi <aun_0@protonmail.ch>
2020-09-07 21:00:49 -0300

    Revert "Upstreamed to v4.4.40"
    This reverts commit c9119ac20eb581fecc3110a7e3408ccd62662cbe, reversing
    changes made to 736b3f34eb354a7783f1526ea46a55ea5b2a096a.
    
    This is the culprit that caused a non-booting kernel image.
    I found a way to appropriately upstream to 4.4.37. After that,
    4.4.40 will be tried again.

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -49,7 +49,9 @@ void __ptrace_link(struct task_struct *child, struct task_struct *new_parent,
  */
 static void ptrace_link(struct task_struct *child, struct task_struct *new_parent)
 {
+	rcu_read_lock();
 	__ptrace_link(child, new_parent, __task_cred(new_parent));
+	rcu_read_unlock();
 }
 
 /**
